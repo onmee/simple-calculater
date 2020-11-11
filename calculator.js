e514362be31604b43159e58158b33917bcbdae6a
SubashA <subashadhikari@outlook.com>
2020-11-11 15:05:51 +0000

    Remove unnecessary variables

--- calculator.js
+++ calculator.js
@@ -1,66 +1,67 @@
-
-
-
-// Create functions that populate the display when you click the number buttons, 
-// you should be storing the 'display value' in a variable for use in the next step
-
-
 // Calculator works as follows:
-   // 1. First number is clicked, which is displayed and stored in a variable.
-   // 2. An operator is clicked, resulting in a function being assigned to a variable.
-   // 3. Second number is clicked, which is displayed and stored in a different variable .
-   // 4. When equals is clicked, the operate function is called and with the above variables.
-   // 5. Clicking AC clears all variables.
+   // 1. First number is clicked, which is displayed and stored in index 0.
+   // 2. An operator is clicked, resulting in that function being atored in index 1.
+   // 3. Second number is clicked, which is displayed and stored in a different index 2.
+   // 4. When equals is clicked, the operate function, which performs the calculatio is called.
+   // 5. AC clears array.
+   // 6. Delete remove last digit.
 
    // Extra : Syntax error when = is pressed without having firstNum -> operand -> secondNum in order
 
 
-//innerHTML set to 0 globally?
+// Default display is 0 
 let form = document.getElementById("display")
 form.innerHTML = 0;
-let firstNum = '';
-let secondNum = '';
 
-// Empty Number Array
+
+// Array to store numbers and operator functions
 let calcArray = [];
 
-// When 0-9 is clicked, show number on the display 
-// Add event listeners for each numeric button, then display the numbers clicked in the inner html
+// Initialise index with empty string before appending, 
+// as otherwise values will be appended to 'undefined'
 
-// If Array is undefined, add number 1st, else use secondNum
-// Or if, Array[0] is not undefined then use secondNum
+calcArray[0] = ""; //First number input
+calcArray[2] = ""; //Second number input
+
+
+// Add event listeners for each numeric button, 
+// then display the numbers clicked in the inner html.
 let numbers = document.querySelectorAll('.number');
 
 numbers.forEach((number => number.addEventListener('click', () => {
  
-    if (calcArray.length == 0 || typeof calcArray[1] === 'undefined') {
-        firstNum += number.textContent
-        calcArray[0] = firstNum
+    if (calcArray[0] === "" || typeof calcArray[1] === 'undefined') {
+        calcArray[0] += number.textContent
         form.innerHTML = calcArray[0]
     } else {
-        secondNum += number.textContent
-        calcArray[2] = secondNum
+        calcArray[2] += number.textContent
         form.innerHTML = calcArray[2]
     }
 } )))
 
 
-// Clear numbers on display when AC is clicked -> Set innerHTML to 0
+// Clear numbers on display when AC is clicked
 let reset = document.querySelector('.clear')
 reset.addEventListener('click', () => {
     form.innerHTML = 0
-    firstNum = ''
-    secondNum = ''
     calcArray = []
-    //firstNum = ''
+    calcArray[0] = ""; 
+    calcArray[2] = ""; 
+
 })
 
-// When delete is clicked, remove the last number
+// When delete is clicked, remove the last digit
 let undo = document.querySelector('.delete')
 undo.addEventListener('click', () => {
-    //Remove last number
-    firstNum = firstNum.slice(0,-1)
-    form.innerHTML = firstNum
+    
+    if (calcArray[0] === "" || calcArray[2] === "") {
+        calcArray[0] = calcArray[0].slice(0,-1)
+        form.innerHTML = calcArray[0]
+    } else {
+        calcArray[2] = calcArray[2].slice(0,-1)
+        form.innerHTML = calcArray[2]
+    }
+
 })
 
 
@@ -105,22 +106,20 @@ let exponent = document.querySelector('.power')
 })
 
 
-
 // When equals is clicked call operate
 let equals = document.querySelector('.equal')
 equals.addEventListener('click', () => {
     calculate();
 })
 
-// Function that calls operate whenever the array is full, and one of the operators is clicked.
+// Function that calls operate whenever the array is full.
 // So that after a number, operator and number are entered, a calculation is performed.
 
 function calculate () {
-    if (typeof calcArray[2] !== 'undefined') {
+    if (calcArray[2] !== "") {
         result = operate(calcArray[0], calcArray[1], calcArray[2])
-        secondNum = ''
-        calcArray[0] = result
-        calcArray[2] = 0
+        calcArray[0] = result.toString()
+        calcArray[2] = ""
         form.innerHTML = calcArray[0]
     }
     else return;
@@ -142,10 +141,3 @@ function multiply(x, y) { return x * y; }
 function divide(x, y) { return x / y; }
 
 function power(x, y) { return x ** y; }
-
-//operate(add, 10, 10);
-//operate("5","5")
-//operate(subtract, 10, 100);
-//operate(multiply, 10, 10);
-//operate(divide, 100, 10);
-//operate(divide, 100, 10);
